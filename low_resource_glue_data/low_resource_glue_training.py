da64b55d6b68fe86257c68ab178bb92a80042034
jmusiel <73840022+jmusiel@users.noreply.github.com>
2021-12-03 20:54:52 +0000

    added saving training to csv file

--- low_resource_glue_data/low_resource_glue_training.py
+++ low_resource_glue_data/low_resource_glue_training.py
@@ -1,6 +1,7 @@
 import os
 import glob
-
+import json
+import pandas as pd
 
 def main():
     tasks = ["RTE","MRPC","STS-B","CoLA"]
@@ -21,11 +22,15 @@ def main():
                     output_dir = "checkpoints/glue/finetuning_script/" + str(training_name)
                     working_dir = "/home/jovyan/joe-cls-vol/class_projects/nlp_11711_project/efficient-tuning-code"
 
+                    train_data_dir = "low_resource_glue_data/" + str(split_str) + "/" + str(rep_str) + "/" + str(task) + "/train.csv"
+                    test_data_dir = "low_resource_glue_data/" + str(split_str) + "/" + str(rep_str) + "/" + str(task) + "/test.csv"
+                    val_data_dir = "low_resource_glue_data/" + str(split_str) + "/" + str(rep_str) + "/" + str(task) + "/dev.csv"
+
                     bash_str = "python -u examples/pytorch/text-classification/run_glue.py "\
                     + "--model_name_or_path bert-base-uncased "\
-                    + "--train_file low_resource_glue_data/" + str(split_str) + "/" + str(rep_str) + "/" + str(task) + "/train.csv "\
-                    + "--test_file low_resource_glue_data/" + str(split_str) + "/" + str(rep_str) + "/" + str(task) + "/test.csv "\
-                    + "--validation_file low_resource_glue_data/" + str(split_str) + "/" + str(rep_str) + "/" + str(task) + "/dev.csv "\
+                    + "--train_file " + str(train_data_dir) + " "\
+                    + "--test_file " + str(test_data_dir) + " "\
+                    + "--validation_file " + str(val_data_dir) + " "\
                     + "--do_train "\
                     + "--do_eval "\
                     + "--do_predict "\
@@ -85,14 +90,65 @@ def main():
                     + "--output_dir " + str(output_dir) + " "\
                     + "2>&1 | tee " + str(output_dir) + "/log.txt"
 
+                    # change directory and run bash command
                     os.chdir(working_dir)
                     os.system(bash_str)
+
+                    # load up dataframes
+                    if os.path.exists("low_resource_glue_data/efficient_finetuning_results.csv"):
+                        results_df = pd.read_csv("low_resource_glue_data/efficient_finetuning_results.csv", index_col=[0])
+                    else:
+                        results_df = pd.DataFrame([])
+
+                    predict_df = pd.read_csv(output_dir+"/predict_results_None.txt", sep="\t")
+                    if task == "CoLA":
+                        test_df = pd.read_csv(test_data_dir, sep=",", header=None)
+                    else:
+                        test_df = pd.read_csv(test_data_dir, sep=",")
+
+                    # load json of eval results
+                    with open(output_dir+"/all_results.json", "r") as f:
+                        eval_results_dict = json.load(f)
+
+                    # compare test results to test set
+                    if task == "CoLA":
+                        label = "1" # 2nd column
+                    elif task == "MRPC":
+                        label = "Quality"
+                    elif task == "RTE":
+                        label = "label"
+                    elif task == "STS-B":
+                        label = "score"
+                    num_diff = test_df[label].compare(predict_df["prediction"]).shape[0]
+                    test_acc = 1 - (num_diff/predict_df.shape[0])
+
+                    # create results dataframe entry
+                    df = pd.DataFrame(
+                        [{
+                            "test_acc": test_acc,
+                            "task": task,
+                            "split": split,
+                            "replicate": replicate,
+                        }]
+                    )
+
+                    # combine results into one dataframe
+                    results_df = results_df.append(df, ignore_index=True)
+
+                    results_df.to_csv("low_resource_glue_data/efficient_finetuning_results.csv")
 
                     os.remove(output_dir+"/pytorch_model.bin")
                     checkpoint_names = glob.glob(output_dir+"/checkpoint*")
                     for check_name in checkpoint_names:
                         os.remove(check_name+"/optimizer.pt")
                         os.remove(check_name+"/pytorch_model.bin")
+
+    i = 0
+    rename_path = "low_resource_glue_data/efficient_finetuning_results" + str(i) + ".csv"
+    while os.path.exists(rename_path):
+        i = i+1
+        rename_path = "low_resource_glue_data/efficient_finetuning_results" + str(i) + ".csv"
+    os.rename("low_resource_glue_data/efficient_finetuning_results.csv", rename_path)
                     
 
 if __name__ == "__main__":
